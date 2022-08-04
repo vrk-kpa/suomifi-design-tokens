e1eb41f266160036d6e48e8fec5b2fc450668b30
Ilkka Pättikangas <ilkka.pattikangas@gmail.com>
2022-08-04 11:58:56 +0300

    Add derived tokens: Gradients, Shadows, Focus, Radius, Transitions

--- src/convert.js
+++ src/convert.js
@@ -2,9 +2,10 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
-const { Command, Option } = require('commander');
+const { Command } = require('commander');
 
 const tokensData = require('./tokens.json');
+const derivedTokensData = require('./derived-tokens.json');
 const tokensParser = require('./tokens-parser');
 const converters = require('./converters');
 
@@ -18,6 +19,97 @@ function main() {
     const opts = program.opts();
 
     const tokensByCategory = tokensParser(tokensData, tokensData.tokens);
+    const derviedTokensByCategory = tokensParser(
+      derivedTokensData,
+      derivedTokensData.tokens,
+    );
+
+    derviedTokensByCategory.forEach((category) => {
+      category.tokens.forEach((derivedToken) => {
+        if (derivedToken.type === 'derived-string') {
+          const variables = derivedToken.value.value.match(/\{.+?\}/g);
+          variables.forEach((variable) => {
+            let strippedVar = variable.replace(/[{} ]/g, '');
+
+            const mainParts = strippedVar.split(',');
+            let variablePart = mainParts[0];
+            const modifierPart = mainParts.length === 2 ? mainParts[1] : null;
+
+            variablePart = variablePart.split('.');
+            const matchingToken = tokensByCategory
+              .find((cat) => cat.category === variablePart[0])
+              .tokens.find((t) => t.name === variablePart[1]);
+            if (!matchingToken) {
+              throw new Error(
+                `Unable to find a matching token for derived token variable ${strippedVar}`,
+              );
+            }
+
+            let derivedValueString = '';
+            if (matchingToken.category === 'colors') {
+              derivedValueString = convertHslMapToString(
+                matchingToken.value,
+                modifierPart,
+              );
+            } else if (matchingToken.category === 'radiuses') {
+              derivedValueString =
+                matchingToken.value.value + matchingToken.value.unit;
+            }
+
+            derivedToken.value.value = derivedToken.value.value.replace(
+              variable,
+              derivedValueString,
+            );
+          });
+        } else if (derivedToken.type === 'derived-object') {
+          const valuesWithVariables = [];
+          // See which values have variables
+          Object.entries(derivedToken.value).forEach((entry) => {
+            const [key, value] = entry;
+            if (String(value).includes('{')) {
+              valuesWithVariables.push({ key: key, value: value });
+            }
+          });
+          valuesWithVariables.forEach((valWithVar) => {
+            const variables = valWithVar.value.match(/\{.+?\}/g);
+            variables.forEach((variable) => {
+              let strippedVar = variable.replace(/[{} ]/g, '');
+
+              const mainParts = strippedVar.split(',');
+              let variablePart = mainParts[0];
+              const modifierPart = mainParts.length === 2 ? mainParts[1] : null;
+
+              variablePart = variablePart.split('.');
+              const matchingToken = tokensByCategory
+                .find((cat) => cat.category === variablePart[0])
+                .tokens.find((t) => t.name === variablePart[1]);
+              if (!matchingToken) {
+                throw new Error(
+                  `Unable to find a matching token for derived token variable ${strippedVar}`,
+                );
+              }
+
+              let derivedValueString = '';
+              if (matchingToken.category === 'colors') {
+                derivedValueString = convertHslMapToString(
+                  matchingToken.value,
+                  modifierPart,
+                );
+              } else if (matchingToken.category === 'radiuses') {
+                derivedValueString =
+                  matchingToken.value.value + matchingToken.value.unit;
+              }
+
+              derivedToken.value[valWithVar.key] = derivedToken.value[
+                valWithVar.key
+              ].replace(variable, derivedValueString);
+            });
+          });
+        }
+      });
+    });
+
+    const allTokens = tokensByCategory.concat(derviedTokensByCategory);
 
     opts.format.split(' ').forEach((format) => {
       if (converters[format] == undefined) {
@@ -25,7 +117,7 @@ function main() {
       } else {
         const exportData = getFormattedTokens(
           converters[format].converters,
-          tokensByCategory,
+          allTokens,
         );
         exportFile(
           `${opts.outdir}`,
@@ -62,6 +154,14 @@ function exportFile(outDir, fileName, data) {
     console.warn(`Writing to file ${outDir}/${fileName} failed!`);
     console.warn(err);
   }
+}
+
+function convertHslMapToString(hslMap, modifier) {
+  if (modifier && modifier.startsWith('alpha-')) {
+    const alphaValue = modifier.substring(6);
+    return `hsla(${hslMap.h}, ${hslMap.s}%, ${hslMap.l}%, ${alphaValue})`;
+  }
+  return `hsl(${hslMap.h}, ${hslMap.s}%, ${hslMap.l}%)`;
 }
 
 main();
--- src/converters/scss/index.js
+++ src/converters/scss/index.js
@@ -18,8 +18,27 @@ function formatToScss(tokensByCategory, scssPrefix) {
       case 'spacing':
         resultArray.push(...formatSpacingToScss(category.tokens, scssPrefix));
         break;
+      case 'radiuses':
+        resultArray.push(...formatSpacingToScss(category.tokens, scssPrefix));
+        break;
+      case 'transitions':
+        resultArray.push(
+          ...formatTransitionToScss(category.tokens, scssPrefix),
+        );
+        break;
+      case 'gradients':
+        resultArray.push(...formatGradientToScss(category.tokens, scssPrefix));
+        break;
+      case 'shadows':
+        resultArray.push(...formatShadowToScss(category.tokens, scssPrefix));
+        break;
+      case 'focuses':
+        resultArray.push(...formatFocusToScss(category.tokens, scssPrefix));
+        break;
       default:
-        console.warn('Unrecognized category type');
+        console.warn(
+          `SCSS formatting: Unrecognized category type ${category.category}`,
+        );
     }
     return resultArray;
   }, []);
@@ -57,6 +76,102 @@ function formatSpacingToScss(tokens, scssPrefix) {
     return `$${scssPrefix}-${token.prefix}-${convertCamelCaseToKebabCase(
       token.name,
     )}: ${token.value.value + token.value.unit};`;
+  });
+}
+
+function formatTransitionToScss(tokens, scssPrefix) {
+  return tokens.map((token) => {
+    return `$${scssPrefix}-${token.prefix}-${convertCamelCaseToKebabCase(
+      token.name,
+    )}: ${token.value.value};`;
+  });
+}
+
+function formatGradientToScss(tokens, scssPrefix) {
+  return tokens.map((token) => {
+    return `$${scssPrefix}-${token.prefix}-${convertCamelCaseToKebabCase(
+      token.name,
+    )}: ${token.value.value};`;
+  });
+}
+
+function formatShadowToScss(tokens, scssPrefix) {
+  return tokens.map((token) => {
+    return `$${scssPrefix}-${token.prefix}-${convertCamelCaseToKebabCase(
+      token.name,
+    )}: ${token.value.value};`;
+  });
+}
+
+function formatFocusToScss(tokens, scssPrefix) {
+  return tokens.map((token) => {
+    let scssString = `@mixin ${scssPrefix}-${
+      token.prefix
+    }-${convertCamelCaseToKebabCase(token.name)} {`;
+
+    if (token.wrapper) {
+      scssString += `${token.wrapper} {`;
+    }
+
+    if (token.value.content) {
+      scssString += `content: ${token.value.content};`;
+    }
+    if (token.value.position) {
+      scssString += `position: ${token.value.position};`;
+    }
+    if (token.value.pointerEvents) {
+      scssString += `pointer-events: ${token.value.pointerEvents};`;
+    }
+    if (token.value.top) {
+      scssString += `top: ${token.value.top.value}${
+        token.value.top.unit !== null ? token.value.top.unit : ''
+      };`;
+    }
+    if (token.value.right) {
+      scssString += `right: ${token.value.right.value}${
+        token.value.right.unit !== null ? token.value.right.unit : ''
+      };`;
+    }
+    if (token.value.bottom) {
+      scssString += `bottom: ${token.value.bottom.value}${
+        token.value.bottom.unit !== null ? token.value.bottom.unit : ''
+      };`;
+    }
+    if (token.value.left) {
+      scssString += `left: ${token.value.left.value}${
+        token.value.left.unit !== null ? token.value.left.unit : ''
+      };`;
+    }
+    if (token.value.borderRadius) {
+      scssString += `border-radius: ${token.value.borderRadius};`;
+    }
+    if (token.value.backgroundColor) {
+      scssString += `background-color: ${token.value.backgroundColor};`;
+    }
+    if (token.value.border) {
+      scssString += `border: ${token.value.border};`;
+    }
+    if (token.value.boxSizing) {
+      scssString += `box-sizing: ${token.value.boxSizing};`;
+    }
+    if (token.value.boxShadow) {
+      scssString += `box-shadow: ${token.value.boxShadow};`;
+    }
+    if (token.value.zIndex) {
+      scssString += `z-index: ${token.value.zIndex};`;
+    }
+    if (token.value.outline) {
+      scssString += `outline: ${token.value.outline};`;
+    }
+    if (token.value.after) {
+      scssString += `&:after {${token.value.after}}`;
+    }
+
+    if (token.wrapper) {
+      scssString += '}';
+    }
+    scssString += '}';
+    return scssString;
   });
 }
 
